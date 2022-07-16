ecf6924130b5d2b6e50b8106ed5ab75a1549fa0b
FatihG34 <fthgnydn34@gmail.com>
2022-07-16 21:58:23 +0300

    button events added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import mailSvg from "./assets/mail.svg";
 import manSvg from "./assets/man.svg";
 import womanSvg from "./assets/woman.svg";
@@ -9,64 +9,96 @@ import phoneSvg from "./assets/phone.svg";
 import padlockSvg from "./assets/padlock.svg";
 import cwSvg from "./assets/cw.svg";
 import Footer from "./components/footer/Footer";
+import axios from "axios";
 
 const url = "https://randomuser.me/api/";
-const defaultImage = "https://randomuser.me/api/portraits/men/75.jpg";
 
 function App() {
+  const [person, setPerson] = useState([]);
+  const [info, setInfo] = useState('');
+  const [show, setShow] = useState(false);
+  const [my, setMy] = useState('');
+
+
+  const setInfoMy = (inf, text) => {
+    setInfo(inf);
+    setMy(text)
+  }
+
+
+
+
+  const getPerson = async () => {
+    try {
+      const { data } = await axios.get(url);
+      setPerson(data.results)
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getPerson()
+  }, [])
+
   return (
     <main>
       <div className="block bcg-orange">
         <img src={cwSvg} alt="cw" id="cw" />
       </div>
       <div className="block">
-        <div className="container">
-          <img src={defaultImage} alt="random user" className="user-img" />
-          <p className="user-title">My ... is</p>
-          <p className="user-value"></p>
-          <div className="values-list">
-            <button className="icon" data-label="name">
-              <img src={womanSvg} alt="user" id="iconImg" />
-            </button>
-            <button className="icon" data-label="email">
-              <img src={mailSvg} alt="mail" id="iconImg" />
-            </button>
-            <button className="icon" data-label="age">
-              <img src={womanAgeSvg} alt="age" id="iconImg" />
-            </button>
-            <button className="icon" data-label="street">
-              <img src={mapSvg} alt="map" id="iconImg" />
-            </button>
-            <button className="icon" data-label="phone">
-              <img src={phoneSvg} alt="phone" id="iconImg" />
-            </button>
-            <button className="icon" data-label="password">
-              <img src={padlockSvg} alt="lock" id="iconImg" />
-            </button>
+        {person?.map((item, index) => (
+          <div className="container" key={index}>
+            <div>
+              <img src={item.picture.medium} alt="random user" className="user-img" />
+              <p className="user-title">My {my} is</p>
+              <p className="user-value" >{info}</p>
+            </div>
+            <div className="values-list">
+              <button className="icon" data-label="name" onClick={() => setInfoMy(item.name.first, 'name')}>
+                <img src={item.gender === 'male' ? manSvg : womanSvg} alt="user" id="iconImg" />
+              </button>
+              <button className="icon" data-label="email" onClick={() => setInfoMy(item.email, 'email')}>
+                <img src={mailSvg} alt="mail" id="iconImg" />
+              </button>
+              <button className="icon" data-label="age" onClick={() => setInfoMy(item.dob.age, 'age')}>
+                <img src={item.gender === 'male' ? manAgeSvg : womanAgeSvg} alt="age" id="iconImg" />
+              </button>
+              <button className="icon" data-label="street" onClick={() => setInfoMy(item.location.country, 'country')}>
+                <img src={mapSvg} alt="map" id="iconImg" />
+              </button>
+              <button className="icon" data-label="phone" onClick={() => setInfoMy(item.phone, 'phone')}>
+                <img src={phoneSvg} alt="phone" id="iconImg" />
+              </button>
+              <button className="icon" data-label="password" onClick={() => setInfoMy(item.login.pass, 'secret password')}>
+                <img src={padlockSvg} alt="lock" id="iconImg" />
+              </button>
+            </div>
+            <div className="btn-group">
+              <button className="btn" type="button" onClick={() => getPerson()} >
+                new user
+              </button>
+              <button className="btn" type="button" onClick={() => setShow(true)}>
+                add user
+              </button>
+            </div>
+            {show &&
+              <table className="table">
+                <thead>
+                  <tr className="head-tr">
+                    <th className="th">Firstname</th>
+                    <th className="th">Email</th>
+                    <th className="th">Phone</th>
+                    <th className="th">Age</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr className="body-tr"></tr>
+                </tbody>
+              </table>
+            }
           </div>
-          <div className="btn-group">
-            <button className="btn" type="button">
-              new user
-            </button>
-            <button className="btn" type="button">
-              add user
-            </button>
-          </div>
-
-          <table className="table">
-            <thead>
-              <tr className="head-tr">
-                <th className="th">Firstname</th>
-                <th className="th">Email</th>
-                <th className="th">Phone</th>
-                <th className="th">Age</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr className="body-tr"></tr>
-            </tbody>
-          </table>
-        </div>
+        ))}
       </div>
       <div style={{ display: "flex", justifyContent: "center" }}>
         <Footer />
