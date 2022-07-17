e745390a312f7330a8975dfd5320fb3f87f8ef10
FatihG34 <fthgnydn34@gmail.com>
2022-07-17 23:28:30 +0300

    finished

--- src/App.js
+++ src/App.js
@@ -18,87 +18,113 @@ function App() {
   const [info, setInfo] = useState('');
   const [show, setShow] = useState(false);
   const [my, setMy] = useState('');
-
+  const [table, setTable] = useState([]);
+  const [pageLoad, setPageLoad] = useState(true)
 
   const setInfoMy = (inf, text) => {
     setInfo(inf);
     setMy(text)
   }
-
-
-
-
   const getPerson = async () => {
     try {
+      if (person) {
+        setPageLoad(false);
+      }
       const { data } = await axios.get(url);
-      setPerson(data.results)
+      setPerson(data.results);
+      setMy('name');
+      setPageLoad(true);
     } catch (error) {
       console.log(error);
     }
+  };
+  const addUser = () => {
+    setShow(true);
+    const check = table.filter((item) => item.email === person[0].email);
+    check.length
+      ? alert("This person is on the list")
+      : setTable([...table, person[0]]);
   };
 
   useEffect(() => {
     getPerson()
   }, [])
 
+  // if (pageLoad) {
+  //   return <div className="page-loading">
+  //     <h1>Page is Loading...</h1>
+  //   </div >
+  // }
   return (
     <main>
       <div className="block bcg-orange">
         <img src={cwSvg} alt="cw" id="cw" />
       </div>
       <div className="block">
-        {person?.map((item, index) => (
-          <div className="container" key={index}>
-            <div>
-              <img src={item.picture.medium} alt="random user" className="user-img" />
-              <p className="user-title">My {my} is</p>
-              <p className="user-value" >{info}</p>
+        <div className="container" >
+          {person?.map((item, index) => (
+            <div key={index}>
+              <div>
+                <img src={item.picture.medium} alt="random user" className="user-img" />
+                <p className="user-title">My {my} is</p>
+                <p className="user-value" >{info}</p>
+              </div>
+              <div className="values-list">
+                <button className="icon" data-label="name" onClick={(e) => setInfoMy(item.name.first, 'name')}>
+                  <img src={item.gender === 'male' ? manSvg : womanSvg} alt="user" id="iconImg" />
+                </button>
+                <button className="icon" data-label="email" onClick={() => setInfoMy(item.email, 'email')}>
+                  <img src={mailSvg} alt="mail" id="iconImg" />
+                </button>
+                <button className="icon" data-label="age" onClick={() => setInfoMy(item.dob.age, 'age')}>
+                  <img src={item.gender === 'male' ? manAgeSvg : womanAgeSvg} alt="age" id="iconImg" />
+                </button>
+                <button className="icon" data-label="street" onClick={() => setInfoMy(item.location.country, 'country')}>
+                  <img src={mapSvg} alt="map" id="iconImg" />
+                </button>
+                <button className="icon" data-label="phone" onClick={() => setInfoMy(item.phone, 'phone')}>
+                  <img src={phoneSvg} alt="phone" id="iconImg" />
+                </button>
+                <button className="icon" data-label="password" onClick={() => setInfoMy(item.login.password, 'secret password')}>
+                  <img src={padlockSvg} alt="lock" id="iconImg" />
+                </button>
+              </div>
             </div>
-            <div className="values-list">
-              <button className="icon" data-label="name" onClick={() => setInfoMy(item.name.first, 'name')}>
-                <img src={item.gender === 'male' ? manSvg : womanSvg} alt="user" id="iconImg" />
-              </button>
-              <button className="icon" data-label="email" onClick={() => setInfoMy(item.email, 'email')}>
-                <img src={mailSvg} alt="mail" id="iconImg" />
-              </button>
-              <button className="icon" data-label="age" onClick={() => setInfoMy(item.dob.age, 'age')}>
-                <img src={item.gender === 'male' ? manAgeSvg : womanAgeSvg} alt="age" id="iconImg" />
-              </button>
-              <button className="icon" data-label="street" onClick={() => setInfoMy(item.location.country, 'country')}>
-                <img src={mapSvg} alt="map" id="iconImg" />
-              </button>
-              <button className="icon" data-label="phone" onClick={() => setInfoMy(item.phone, 'phone')}>
-                <img src={phoneSvg} alt="phone" id="iconImg" />
-              </button>
-              <button className="icon" data-label="password" onClick={() => setInfoMy(item.login.pass, 'secret password')}>
-                <img src={padlockSvg} alt="lock" id="iconImg" />
-              </button>
-            </div>
-            <div className="btn-group">
-              <button className="btn" type="button" onClick={() => getPerson()} >
-                new user
-              </button>
-              <button className="btn" type="button" onClick={() => setShow(true)}>
-                add user
-              </button>
-            </div>
-            {show &&
-              <table className="table">
-                <thead>
-                  <tr className="head-tr">
-                    <th className="th">Firstname</th>
-                    <th className="th">Email</th>
-                    <th className="th">Phone</th>
-                    <th className="th">Age</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr className="body-tr"></tr>
-                </tbody>
-              </table>
-            }
+          ))}
+          <div className="btn-group">
+            <button className="btn" type="button" onClick={() => getPerson()} >
+              {pageLoad ? 'new user' : 'Loading...'}
+            </button>
+            <button className="btn" type="button" onClick={addUser}>
+              add user
+            </button>
           </div>
-        ))}
+          {show &&
+            <table className="table">
+              <thead>
+                <tr className="head-tr">
+                  <th className="th">Firstname</th>
+                  <th className="th">Email</th>
+                  <th className="th">Phone</th>
+                  <th className="th">Age</th>
+                </tr>
+              </thead>
+              <tbody>
+                {table?.map((item2, index2) => {
+                  return (
+                    <tr className="body-tr" key={index2}>
+                      <td>{item2.name.first}</td>
+                      <td>{item2.email}</td>
+                      <td>{item2.phone}</td>
+                      <td>{item2.dob.age}</td>
+                    </tr>
+                  )
+                })}
+              </tbody>
+            </table>
+          }
+        </div>
+
       </div>
       <div style={{ display: "flex", justifyContent: "center" }}>
         <Footer />
@@ -106,5 +132,4 @@ function App() {
     </main>
   );
 }
-
 export default App;
